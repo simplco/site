6e9e1b9ca38c1cac67b130465e721d4b46edaa46
draganlukic <draganlukic15@yahoo.com>
2019-07-07 20:00:33 -0700

    changes

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
     return(
-        <div id="Navbar">
-            navbars
+        <div id="Navbar" className="Navbar">
+              <b> simpl. </b>
+              <a> contact </a>
+              <a> about us </a>
         </div>
     )
 }
