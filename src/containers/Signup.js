afeecb983eff8741666653f5af0286edd0e55c55
draganlukic <draganlukic15@yahoo.com>
2019-07-13 22:57:31 -0700

    updated design

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -3,26 +3,32 @@ import './Signup.css';
 
 function Signup() {
     return( 
-     <div class="row">
-       <div class="column">
-          <div class="column1">
-              <a>Lets chat</a>
-              <p>420BlazeIt@simpl.co</p>
-          </div>
-       </div>
-       <div class="column">
-          <div class="column2">
-              <div class="column2a">
-                  <a>name: </a>
-                  <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
-              </div>
-              <div class="column2b">
-                  <a>email: </a>
-                  <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
-              </div>
-            </div>
-        </div> 
-     </div>
+            <div class="row">  
+                <div class="column">
+                    <div class="column1">
+                     <p>Lets chat.</p>
+                     <a>Hi@simplco.com</a>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="column2">
+                       <div class="column2half">
+                            <div class="column2a">
+                                 <a>name: </a>
+                                 <input type="text" id="fname" name="firstname"></input>
+                            </div>
+                             <div class="column2b">
+                                 <a>email: </a>
+                                 <input type="text" id="fname" name="firstname"></input>
+                             </div>
+                        </div>
+                    </div> 
+                    <div class="column2">
+
+                    </div>  
+                </div>
+            </div> 
+    
 
 
     )
